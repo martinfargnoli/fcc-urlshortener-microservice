949c1269294b85b7402a8848d08286363a2f96d3
Martín Fargnoli <mfargnoli@outlook.com>
2024-01-20 17:24:59 -0300

    Proyecto terminado.

--- index.js
+++ index.js
@@ -2,23 +2,60 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongodb = require("mongodb");
+const dns = require("dns");
+const urlparser = require("url");
+
+// Connection to the database.
+const client = new mongodb.MongoClient(process.env.MONGODB_URI);
+const db = client.db("urlshortener");
+const urls = db.collection("urls");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+// Endpoint to create short URLs.
+app.post("/api/shorturl", function (req, res) {
+  const dnsLookup = dns.lookup(
+    urlparser.parse(req.body.url).hostname,
+    async (err, address) => {
+      if (!address) {
+        res.json({ error: "invalid url" });
+      } else {
+        const urlNum = await urls.countDocuments({});
+        const urlDoc = {
+          url: req.body.url,
+          short_url: urlNum,
+        };
+        const result = await urls.insertOne(urlDoc);
+        res.json({
+          original_url: req.body.url,
+          short_url: urlNum,
+        });
+      }
+    },
+  );
 });
 
-app.listen(port, function() {
+// Endpoint to redirect to the original URL using the short URL.
+app.get("/api/shorturl/:short_url", async (req, res) => {
+  const urlDoc = await urls.findOne({ short_url: +req.params.short_url });
+  if (urlDoc) {
+    res.redirect(urlDoc.url);
+  } else {
+    res.json({ error: "URL not found." });
+  }
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
